ea2bc8f836d0a17849373239928d762e5f9c8c76
xxanqw <potik217@gmail.com>
2024-01-28 17:59:03 +0200

    Консольний проект

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Будь ласка введіть два числа через пробіл: ");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+            int num1 = Int32.Parse(numbers[0]);
+            int num2 = Int32.Parse(numbers[1]);
+            int sum = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} дорівнює {sum}.");
+        }
+    }
+}
